2e0d3aff005ec33b21a500e1006ce4cc026b162b
Georgiy Malyukov <GDXRepo@users.noreply.github.com>
2016-09-12 22:08:09 +0400

    Create GJSON.swift

---
+++ GJSON.swift
@@ -0,0 +1,101 @@
+enum GJSONTypes {
+    case number
+    case string
+    case bool
+    case array
+    case object
+}
+
+class GJSON {
+    
+    let json: Any?
+    
+    init(_ json: Any?) {
+        self.json = json
+    }
+    
+    func valueAt(_ path: String, of type: GJSONTypes) throws -> Any {
+        let object = GJSON.path(path, json: json)
+        
+        switch type {
+        case .number:
+            return object as! NSNumber
+        case .string:
+            return object as! String
+        case .bool:
+            return object as! Bool
+        case .array:
+            return object as! [Any]
+        case .object:
+            return object as! [String: Any]
+        }
+    }
+    
+    func num(_ path: String) throws -> NSNumber {
+        return nullable(path) as! NSNumber
+    }
+    
+    func str(_ path: String) throws -> String {
+        return nullable(path) as! String
+    }
+    
+    func bool(_ path: String) throws -> Bool {
+        return nullable(path) as! Bool
+    }
+    
+    func arr(_ path: String) throws -> [Any] {
+        return nullable(path) as! [Any]
+    }
+    
+    func obj(_ path: String) throws -> [String: Any] {
+        return nullable(path) as! [String: Any]
+    }
+    
+    func nullable(_ path: String) -> Any? {
+        return GJSON.path(path, json: json)
+    }
+    
+    static func path(_ path: String, json: Any) -> Any? {
+        let comps = path.trimmingCharacters(in: CharacterSet(charactersIn: "/")).components(separatedBy: "/")
+        var current: Any? = json
+        
+        for (i, comp) in comps.enumerated() {
+            // check special syntax
+            if comp.hasPrefix(":") {
+                // get array item index
+                let indexRequest = comp.substring(from: comp.index(after: comp.startIndex))
+                
+                if let arrayIndex = Int(indexRequest) { // try to read element as array
+                    if let asArray = current as? [Any] { // it's an array?
+                        if arrayIndex < asArray.count { // we are inside its bounds?
+                            current = asArray[arrayIndex] // then proceed
+                        }
+                        else { // outside of bounds?
+                            print("invalid item index \"\(arrayIndex)\" in \(current!)")
+                            return nil
+                        }
+                    }
+                    else { // not an array?
+                        print("item is not an array \"\(comp)\" in \(current!)")
+                        return nil
+                    }
+                }
+                else { // not a numeric index?
+                    print("invalid item index \"\(indexRequest)\" in \(current!)")
+                    return nil
+                }
+            }
+            else if i < comps.count { // we're still inside our components?
+                if let unwrapped = current as? [String: Any] { // we can proceed for dictionary?
+                    current = unwrapped[comp] // unwrap current item and repeat
+                }
+                else { // can't read a dictionary?
+                    print("item is not a dictionary \"\(comp)\" in \(current!)")
+                    return nil
+                }
+            }
+        }
+        return current
+    }
+    
+}
